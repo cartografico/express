1aa1f61de85f885773bd9302044f34d68a896973
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-29 12:43:14 -0500

    Excercise N. 3

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+  let nombre = req.query.nombre;
+  if (nombre) {
+    res.send(`<h1>Hola ${nombre}</h1>`);
+  } else {
+    res.send(`<h1>Hola desconocido!</h1>`);
+  }
+
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
