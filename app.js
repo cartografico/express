9ee9edef0bab8ea0dd7db26199aaf23a5523915d
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-29 12:44:28 -0500

    ! added

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ app.listen(3000, () => console.log('Listening on port 3000!'));
 app.get('/', (req, res) => {
   let nombre = req.query.nombre;
   if (nombre) {
-    res.send(`<h1>Hola ${nombre}</h1>`);
+    res.send(`<h1>Hola ${nombre}!</h1>`);
   } else {
     res.send(`<h1>Hola desconocido!</h1>`);
   }
 
 });
-
