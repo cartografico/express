525f3d34f45e4ad2047495bae3ff5f078a187544
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-30 11:50:59 -0500

    debugging error 200

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
 
 app.get('/user/:name', (req, res) => {
   let name = req.params.name;
   res.send(`<h1>Hola ${name.charAt(0).toUpperCase() + name.slice(1)}!</h1>`);
 })
 
+app.listen(3000, () => console.log('Listening on port 3000!'));
 
