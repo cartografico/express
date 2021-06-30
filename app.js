3dd2d7006c0a182e3aa0270cb3cb7727bf06a3db
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-30 11:49:08 -0500

    to uppercase fixed

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ app.listen(3000, () => console.log('Listening on port 3000!'));
 
 app.get('/user/:name', (req, res) => {
   let name = req.params.name;
-  name.toString();
-  res.send('Hola ' + name.charAt(0).toUpperCase() + name.slice(1));
+  res.send(`<h1>Hola ${name.charAt(0).toUpperCase() + name.slice(1)}!</h1>`);
 })
 
 
