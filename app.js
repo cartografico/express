13a26e53d611a27c317c12c5f66e125531065ff4
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-30 11:55:10 -0500

    error 200

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 
-
-app.get('/user/:name', (req, res) => {
-  let name = req.params.name;
-  res.send(`<h1>Hola ${name.charAt(0).toUpperCase() + name.slice(1)}!</h1>`);
+app.get('/user/:nombre', (req, res) => {
+  let nombre = req.params.nombre;
+  res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase() + nombre.slice(1)}!</h1>`);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
