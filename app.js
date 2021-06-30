47b5f934c36656ebd458f5bd0cecd9a95e820525
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-06-30 11:46:53 -0500

    First letter to uppercase

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
-app.get('/', (req, res) => {
-  let nombre = req.query.nombre;
-  if (nombre) {
-    res.send(`<h1>Hola ${nombre}!</h1>`);
-  } else {
-    res.send(`<h1>Hola desconocido!</h1>`);
-  }
+app.get('/user/:name', (req, res) => {
+  let name = req.params.name;
+  name.toString();
+  res.send('Hola ' + name.charAt(0).toUpperCase() + name.slice(1));
+})
 
-});
+
