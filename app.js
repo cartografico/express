a1581f111a22a34ab869e943ea51d8ff9eb2bbcf
Sigfrid Blake <sigfrid@MacBook-Pro-de-Sigfrid.local>
2021-07-01 01:09:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get('/makers/:nombre', (req, res) => {
-  let nombre = req.params.nombre;
-  res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase() + nombre.slice(1)}!</h1>`);
-})
+app.get('/', (req, res) => {
+  let number;
+  for (number = 0; number < 50; number++ ){
+    console.log(`<h1>${number} es par!</h1>`);
+  }
+});
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
 
