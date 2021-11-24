250cdf1ece71e4c46e91b486223b76627f2dd652
Sigfrid <Blake>
2021-11-24 16:26:32 -0500

    listening port 3000

--- primera-app-express/app.js
+++ primera-app-express/app.js
@@ -1,72 +1,8 @@
-'use strict';
+const express = require('express');
+const app = express();
 
-const gatos = [
-    {
-        id : 1,
-        nombre: "Jim",
-    },
-    {
-        id: 2,
-        nombre : "Dante"
-    },
-    {
-        id:3,
-        nombre: "Olivia"
-    },
-];
-
-const weight = [
-    {
-        id : 1,
-        weight: 7,
-    },
-    {
-        id: 2,
-        weight : 4.8,
-    },
-    {
-        id:3,
-        weight: 3.5,
-    },
-];
-const id = 3;
-const getCat = (id, callback) =>{
-    const cat = gatos.find( (e) =>{
-        return e.id === id;
-    })?.nombre;
-
-    if (cat){
-        callback (null, cat);
-    } else {
-        callback (`ERROR`);
-    }
-};
-getCat(id, (err, cat) => {
-    if (err){
-        console.log("error en la consulta")
-        return console.log(err)
-    }  
-    return console.log(cat);
-    }   
-);
-
-const getWeigth = (id, callback) =>{
-    const gato = weight.find((e)=>{
-        if (e.id === id){
-            return e.id;
-        };
-    })?.weight;
-
-    if (gato) {
-        callback (null,gato);
-    } else {
-        callback ("No hay michi");
-    };
-};
-
-getWeigth(id, (err, weight)=>{
-    if (err){
-        return console.log(err)
-    }
-    return console.log(weight)
+app.get('/', (req, res) => {
+  res.send('<h1>Hola Mundo</h1>');
 });
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
