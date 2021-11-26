a5aa717696be36b435d9371b378d16502c5f8f25
Sigfrid <Blake>
2021-11-26 18:25:22 -0500

    reto saludame 3

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const bodyParser = require('body-parser')
 
-app.use(express.static(path.join(__dirname, 'public')));
+// const path = require("path");
 
-app.get('/', (req, res) => {
-  res.send('hola sumercé')
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render('index' );
 });
 
-console.log(path.basename)
-
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log("Listening on port 3000!"));
