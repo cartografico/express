350a47014dd226ecbf6c601649bd3337f7378c71
Sigfrid <Blake>
2021-11-27 12:30:17 -0500

    urlencoded added

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
 
-// const path = require("path");
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
     res.render('index' );
+    next();
 });
+
+app.post('/',(req, res,next) => {
+    const nameuser = req.body.name;
+    console.log(req.body.name);
+    res.render('response', { name: "tarado" });
+    next()
+})
+
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
