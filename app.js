2bebf80604ccf8e7a999be08d2cd8116625d90a6
Sigfrid <Blake>
2021-11-28 01:09:54 -0500

    userAgent Browser

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const app = express();
+const useragent = require('express-useragent');
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.set("view engine", "pug");
-
-app.get("/", (req, res, next) => {
-    res.render('index' );
-    next();
+app.use(useragent.express());
+app.get('/', function(req, res){
+    res.send(req.useragent.browser);
 });
-function capitalizeFirstLetter(str) {
-    return str[0].toUpperCase() + str.slice(1);
-}
 
-app.post('/',(req, res,next) => {
-    const username = req.body.name;
-    const text = capitalizeFirstLetter(username);
-    const nombre = text;
-    res.render('response', { name: nombre, empty:" " });
-    console.log(nombre)
-    next()
-})
-
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(3000, () => console.log('Listening on port 3000!'));
