31d16fc165db99abe3b18f5246d00af2d2f7c60e
Sigfrid <Blake>
2021-12-01 01:36:37 -0500

    mongo start

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    const datos = req.headers['user-agent'];
-    res.send(datos)
-    console.log(datos);
+app.get('/', (req, res)=>{
+    const user = req.query.name
+    res.send("Hola " + user);
+
 });
 
-
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, ()=>{
+    console.log("Escuchando el puerto 3000!")
+})
