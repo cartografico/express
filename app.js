bd75891df6d915c08bce5469290b1c65f487bb33
Sigfrid <Blake>
2021-12-01 16:19:01 -0500

    first attempt

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URL;
+const date = Date()
+
+mongoose.connect('mongodb://127.0.0.1:27017', 
+{ useNewUrlParser: true })
+
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+  });
+
 
 
 app.get('/', (req, res)=>{
-    const user = req.query.name
-    res.send("Hola " + user);
+    const user = req.query.user + "";
 
+    if (!user){
+        user = "Anónimo"
+    }
+    const schema = mongoose.Schema({
+        date: Date,
+        name : String ,
+    });
+    const Visitor = mongoose.model('Visitor', schema);
+    Visitor.create({date : date, name : user})
+
+    console.log('Tutto a posto')
 });
+
+
 
 app.listen(3000, ()=>{
     console.log("Escuchando el puerto 3000!")
-})
+});
